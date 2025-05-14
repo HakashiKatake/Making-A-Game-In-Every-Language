9c31290a867ae18a74d7f83dca8d2496894a3090
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-05-14 21:30:29 +0530

    add: made aliens shoot bullets

--- Python-Pygame/main.py
+++ Python-Pygame/main.py
@@ -13,6 +13,8 @@ screen_height = 800
 #game vars
 rows = 5
 cols = 5
+alien_cooldown = 1000
+last_alien_shot = pygame.time.get_ticks()
 
 #colors
 red = (255, 0, 0)
@@ -73,6 +75,7 @@ class Bullets(pygame.sprite.Sprite):
             self.kill()
 
 
+
 #crate aliens 
 
 class Aliens(pygame.sprite.Sprite):
@@ -92,7 +95,18 @@ class Aliens(pygame.sprite.Sprite):
             self.move_direction *= -1
             self.move_counter *= self.move_direction
 
-        
+
+class Alien_Bullets(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('img/alien_bullet.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = [x ,y]
+
+    def update(self):
+        self.rect.y += 2
+        if self.rect.top > screen_height:
+            self.kill()
 
 
 
@@ -102,6 +116,7 @@ class Aliens(pygame.sprite.Sprite):
 spaceship_group = pygame.sprite.Group()
 bullet_group = pygame.sprite.Group()
 alien_group = pygame.sprite.Group()
+alien_bullet_group = pygame.sprite.Group()
 
 def create_aliens():
     for row in range(rows):
@@ -130,6 +145,14 @@ while run:
     #draw background
     draw_bg()
 
+    #create random alien bullets
+    time_now = pygame.time.get_ticks()
+    if time_now - last_alien_shot > alien_cooldown and len(alien_bullet_group) < 5 and len(alien_group) > 0:
+        attacking_alien = random.choice(alien_group.sprites())
+        alien_bullet = Alien_Bullets(attacking_alien.rect.centerx, attacking_alien.rect.bottom)
+        alien_bullet_group.add(alien_bullet)
+        last_alien_shot = time_now
+
     #event handling
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -141,11 +164,13 @@ while run:
     #update sprite groups
     bullet_group.update()
     alien_group.update()
+    alien_bullet_group.update()
 
     #draw sprite groups
     spaceship_group.draw(screen)
     bullet_group.draw(screen)
     alien_group.draw(screen)
+    alien_bullet_group.draw(screen)
 
     pygame.display.update()
 
