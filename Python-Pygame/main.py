320521a999f71088502ae9bd592943a6698852a0
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-05-14 20:30:13 +0530

    add: bg function in pygame

--- Python-Pygame/main.py
+++ Python-Pygame/main.py
@@ -6,12 +6,22 @@ from pygame.locals import *
 screen_width = 600
 screen_height = 800
 
+bg = pygame.image.load('img/bg.png')
+
+def draw_bg():
+    screen.blit(bg, (0,0))
+
 
 screen = pygame.display.set_mode((screen_width, screen_height)) 
 pygame.display.set_caption("Pygame Window") 
 
 run = True
 while run:
+
+    #draw background
+    draw_bg()
+
+    #event handling
     for event in pygame.event.get():
         if event.type == QUIT:
             run = False
